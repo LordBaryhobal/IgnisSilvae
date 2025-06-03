79e7bca9cc818b2199888ebb2ee8a64ca108691d
LordBaryhobal <lordbaryhobal@gmail.com>
2025-06-03 13:29:57 +0200

    feat: add layers + improve display

--- src/main/scala/visual/App.scala
+++ src/main/scala/visual/App.scala
@@ -2,13 +2,18 @@ package visual
 
 import ch.hevs.gdx2d.desktop.PortableApplication
 import ch.hevs.gdx2d.lib.GdxGraphics
+import com.badlogic.gdx.Input.Keys
 import com.badlogic.gdx.graphics.Pixmap.Format
-import com.badlogic.gdx.graphics.{Color, Pixmap}
+import com.badlogic.gdx.graphics.{Color, Pixmap, Texture}
 import com.badlogic.gdx.{Files, Gdx}
-import core.{State, World}
+import core.{Cell, State, World}
 import org.lwjgl.opengl.Display
+import visual.Layer.Layer
 
+import java.io.{FileOutputStream, PrintWriter}
 import java.nio.ByteBuffer
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 
 class App extends PortableApplication(800, 800) {
@@ -16,8 +21,15 @@ class App extends PortableApplication(800, 800) {
   private val iconSizes: Array[Int] = Array(16, 32, 48, 64, 128, 256)
   System.setProperty("LWJGL_WM_CLASS", TITLE) // See https://github.com/LWJGL/lwjgl/issues/67
 
+  private val size = 8
+
+  private val textures: mutable.Map[Color, Texture] = new mutable.HashMap()
+
   private var world: World = World.make(100, 100)
   private var i: Int = 1
+  private var layer: Layer = Layer.ALL
+  private val fireDensity: ArrayBuffer[Double] = new ArrayBuffer[Double]()
+  logFireDensity()
 
   override def onInit(): Unit = {
     setTitle(TITLE)
@@ -28,31 +40,39 @@ class App extends PortableApplication(800, 800) {
     g.clear()
     val winWidth: Float = getWindowWidth
     val winHeight: Float = getWindowHeight
-    val size: Float = Math.min(winWidth / world.width, winHeight / world.height)
+    //val size: Float = Math.min(winWidth / world.width, winHeight / world.height)
     val ox: Float = (winWidth - (world.width - 1) * size) / 2
     val oy: Float = (winHeight - (world.height - 1) * size) / 2
     world.grid.zipWithIndex.foreach(p1 => {
       val y: Int = p1._2
       p1._1.zipWithIndex.foreach(p2 => {
         val x: Int = p2._2
-        val color: Color = p2._1.state match {
-          case State.ALIVE => Color.GREEN
-          case State.FIRE => Color.ORANGE
-          case State.DEAD => Color.BLACK
-          case _ => Color.GRAY
-        }
-        g.drawFilledRectangle(ox + x * size, oy + y * size, size, size, 0f, color)
+        val color: Color = getCellColor(p2._1)
+        val texture: Texture = getColorTexture(color)
+        g.draw(texture, ox + x * size, oy + y * size)
       })
     })
 
+    g.drawString(0f, 800f, "Layer: " + (layer match {
+      case Layer.ALL => "All"
+      case Layer.STATE => "State"
+      case Layer.FIRE_PROBABILITY => "Fire probability"
+      case Layer.GROWTH_PROBABILITY => "Growth probability"
+      case Layer.HUMIDITY => "Humidity"
+    }))
+
     g.drawFPS()
+
     if (i == 0) {
       world = world.step()
+      world.printStats()
+      logFireDensity()
     }
 
     i += 1
     //i %= 60
-    i %= 10
+    i %= 5
+
   }
 
   private def setIcons(paths: Array[String]): Unit = {
@@ -61,7 +81,6 @@ class App extends PortableApplication(800, 800) {
       var pixmap: Pixmap = new Pixmap(Gdx.files.getFileHandle(path, Files.FileType.Internal))
       if (pixmap.getFormat != Format.RGBA8888) {
         val rgba: Pixmap = new Pixmap(pixmap.getWidth, pixmap.getHeight, Format.RGBA8888)
-        //rgba.setBlending(Blending.None)
         rgba.drawPixmap(pixmap, 0, 0)
         pixmap.dispose()
         pixmap = rgba
@@ -71,6 +90,72 @@ class App extends PortableApplication(800, 800) {
       pixmap.dispose()
     }
     Display.setIcon(icons)
+  }
+
+  def getColorTexture(color: Color): Texture = {
+    textures.get(color) match {
+      case Some(texture) => texture
+      case None => {
+        val pixmap: Pixmap = new Pixmap(size, size, Format.RGB888)
+        pixmap.setColor(color)
+        pixmap.fill()
+        val texture: Texture = new Texture(pixmap)
+        textures.put(color, texture)
+        texture
+      }
+    }
+  }
+
+  def getCellColor(cell: Cell): Color = {
+    layer match {
+      case Layer.ALL => {
+        cell.state match {
+          case State.ALIVE => Color.GREEN
+          case State.FIRE => Color.ORANGE
+          case State.DEAD => Color.BLACK
+          case _ => Color.GRAY
+        }
+      }
+      case Layer.STATE => {
+        cell.state match {
+          case State.ALIVE => Color.GREEN
+          case State.FIRE => Color.ORANGE
+          case State.DEAD => Color.BLACK
+          case _ => Color.GRAY
+        }
+      }
+      case Layer.FIRE_PROBABILITY => {
+        Color.GREEN.cpy().lerp(Color.RED, cell.properties.fireProbability.toFloat)
+      }
+      case Layer.GROWTH_PROBABILITY => {
+        Color.GRAY.cpy().lerp(Color.GREEN, cell.properties.growthProbability.toFloat)
+      }
+      case Layer.HUMIDITY => {
+        Color.WHITE.cpy().lerp(Color.BLUE, cell.properties.humidity.toFloat)
+      }
+    }
+  }
+
+  override def onKeyDown(keycode: Int): Unit = {
+    if (keycode == Keys.NUM_1 || keycode == Keys.A) {layer = Layer.ALL}
+    else if (keycode == Keys.NUM_2 || keycode == Keys.S) {layer = Layer.STATE}
+    else if (keycode == Keys.NUM_3 || keycode == Keys.F) {layer = Layer.FIRE_PROBABILITY}
+    else if (keycode == Keys.NUM_4 || keycode == Keys.G) {layer = Layer.GROWTH_PROBABILITY}
+    else if (keycode == Keys.NUM_5 || keycode == Keys.H) {layer = Layer.HUMIDITY}
+    else if (keycode == Keys.E) {exportStats()}
+  }
+
+  def logFireDensity(): Unit = {
+    val density: Double = world.getFireDensity
+    fireDensity.addOne(density)
+  }
+
+  def exportStats(): Unit = {
+    val writer: PrintWriter = new PrintWriter(new FileOutputStream("fire_density.csv"))
+    fireDensity.foreach(d => {
+      writer.println(d)
+    })
+    writer.close()
   }
 }
 
---
+++ src/main/scala/visual/Layer.scala
@@ -0,0 +1,6 @@
+package visual
+
+object Layer extends Enumeration {
+  type Layer = Value
+  val ALL, STATE, FIRE_PROBABILITY, GROWTH_PROBABILITY, HUMIDITY = Value
+}
